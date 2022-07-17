8f20063657b19345f36be13419e7cb543e5d16f6
elluis <elluis.codes@gmail.com>
2022-07-17 12:30:17 -0500

    nested single file set-up 3_deep
    displaying random img from amazon books

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 import ReactDom from 'react-dom'
 
 
-
-
-
-function Greeting() {
-
-
-    return <h1>hi</h1>
+function BookList() {
+    return (
+        <section>
+            <Book/>
+            <Book/>
+            <Book/>
+            <Book/>
+            <Book/>
+            <Book/>
+            <Book/>
+            <Book/>
+            <Book/>
+        </section>
+    )
 }
 
-ReactDom.render(<Greeting />, document.getElementById('root'));
+
+const Book = () => {
+    return <article><Image/></article>;
+}
+
+
+const Image = () => <img
+    src="https://images-na.ssl-images-amazon.com/images/I/51WS36aA2BL._SX387_BO1,204,203,200_.jpg"/>
+
+ReactDom.render(<BookList/>, document.getElementById('root'));
